1c602373f4a3abfc7f8b1176aa483748d220e48b
noob-ubaid <ubaidurrahman661@gmail.com>
2025-12-11 20:42:02 +0600

    added social link logo

--- app/components/TechButton/TechButton.tsx
+++ app/components/TechButton/TechButton.tsx
@@ -9,9 +9,9 @@ interface TechProps {
 const TechButton: React.FC<TechProps> = ({ icon: Icon, name, href }) => {
   return (
     <a
-    target="_blank"
+      target="_blank"
       href={href}
-      className="inline-flex mx-1 mt-1.5 shadow-[inset_0_0_15px_rgba(0,0,0,0.15)] items-center border-dashed dark:text-white text-sm font-bold border-2 py-0.5 px-1.5 text-black rounded-lg bg-second dark:bg-second-dark gap-2"
+      className="inline-flex  mt-1.5 shadow-[inset_0_0_15px_rgba(0,0,0,0.15)] items-center border-dashed dark:text-white text-sm font-bold border-2 py-0.5 px-1.5 text-black rounded-lg bg-second dark:bg-second-dark gap-2"
     >
       <Icon className="w-4 h-4" /> {name}
     </a>
