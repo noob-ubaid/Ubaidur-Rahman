d1770ac11fb61aa1f7a7e3b2fa974d8b23ae4c5e
noob-ubaid <ubaidurrahman661@gmail.com>
2026-01-08 20:11:57 +0600

    updated time function

--- app/components/MoreAboutMe/MoreAboutMe.tsx
+++ app/components/MoreAboutMe/MoreAboutMe.tsx
@@ -1,13 +1,27 @@
-import React from "react";
+"use client";
+import React, { useEffect, useState } from "react";
 import { GrLocation } from "react-icons/gr";
 import { MdCall } from "react-icons/md";
 import Title from "../shared/Title";
 import { MdOutlineMail } from "react-icons/md";
 import { MdAccessTime } from "react-icons/md";
 const MoreAboutMe = () => {
-  const time = new Date().toLocaleTimeString("en-US", {
-    timeZone: "Asia/Dhaka",
-  });
+  const [time, setTime] = useState("00:00 AM");
+
+  useEffect(() => {
+    const updateTime = () => {
+      const bdTime = new Date().toLocaleTimeString("en-US", {
+        timeZone: "Asia/Dhaka",
+        hour12: true,
+      });
+      setTime(bdTime);
+    };
+
+    updateTime();
+    const interval = setInterval(updateTime, 1000);
+
+    return () => clearInterval(interval);
+  }, []);
   const moreAboutMe = [
     {
       icon: <GrLocation size={19} color="gray" />,
@@ -29,7 +43,7 @@ const MoreAboutMe = () => {
   return (
     <div className=" mt-12">
       <Title upperText="Other" lowerText="Information" />
-      <div className="grid grid-cols-12 gap-2 mt-4">
+      <div className="grid grid-cols-12 gap-3 mt-5">
         {moreAboutMe.map((about, idx) => (
           <div key={idx} className="col-span-12 sm:col-span-6">
             <div className="flex items-center gap-3">
