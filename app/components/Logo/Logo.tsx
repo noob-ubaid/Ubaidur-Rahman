891504d4a6ab8c0c398e595120a550c9c9ee9659
noob-ubaid <ubaidurrahman661@gmail.com>
2026-01-22 20:22:07 +0600

    resize the avatar

--- app/components/Logo/Logo.tsx
+++ app/components/Logo/Logo.tsx
@@ -1,17 +1,18 @@
-import { Lora } from "next/font/google";
 import Image from "next/image";
 import Link from "next/link";
 
-const lora = Lora({ subsets: ["latin"], weight: "700" });
-
 export default function Logo() {
-  const name = "<Ubaidur/>";
   return (
     <Link
       href={"/"}
-      className={`${lora.className} text-[28px] italic font-bold`}
     >
-      <Image width={56} height={56} className="object-cover bg-center rounded-md" src={"/avatar.png"} alt="Logo" />
+      <Image
+        width={48}
+        height={48}
+        className="object-cover bg-center rounded-full"
+        src={"/avatar.png"}
+        alt="Logo"
+      />
     </Link>
   );
 }
